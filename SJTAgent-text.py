5b63c18f21231eb449bd35f7441db45541f2ab81
colehank <1184270316@qq.com>
2025-09-30 20:03:29 +0800

    feat: prompt modify

--- SJTAgent-text.py
+++ SJTAgent-text.py
@@ -3,12 +3,14 @@ from src import SJTRunner, DataLoader
 #%%
 N_ITEM = 4 #每个自陈题生成多少道sjt题
 SCALE = "NEO-PI-R" #使用的量表
-TRAITS = ["O4", "C4", "E5", "A5", "N5"] #选取特质
-SITUATION_THEME = "大学校园里的日常生活" #情境主题
+TRAITS = ["O4", "C4", "E2", "A5", "N4"] #选取特质
+SITUATION_THEME = "日常生活中的普遍情景" #情境主题
+TARGET_POPULATION = '普通成年人' #目标人群
+MODEL = 'deepseek-v3.1-thinking' #使用的基座LLM
+
 RESULT_DIR = 'output'  # 保存结果的目录
 RESULT_SJT_FN = 'sjt-text' # 保存结果的文件名
 RESULT_DETAILED_SJT_FN = 'sjt-text_detailed' # 保存结果的详细文件名
-MODEL = 'gpt-5-mini' #使用的基座LLM
 
 data_loader = DataLoader()# 数据加载器
 neopir = data_loader.load(SCALE, 'zh')# 这里是加载NEO-PI-R数据，zh指中文，可以en即英文
@@ -25,7 +27,8 @@ available_traits_names = [f'{neopir[trait]['facet_name']}' for trait in availabl
 runner = SJTRunner(
     situation_theme=SITUATION_THEME,
     scale=neopir,
-    meta=neopir_meta
+    meta=neopir_meta,
+    target_population='普通成年人',
     )
 
 all_items = runner.cook_async(
